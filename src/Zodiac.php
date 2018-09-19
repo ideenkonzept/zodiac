fb604d9a2a6ef77e457f0044da1dcc89400c07c4
Ahmed Dabak <11370582+ahmeddabak@users.noreply.github.com>
2018-09-19 08:39:12 +0200

    add milliseconds to fix month not found for the end of sat 22.9.2018

--- src/Zodiac.php
+++ src/Zodiac.php
@@ -24,7 +24,7 @@ abstract class Zodiac {
 	}
 
 	public function endDate() {
-		return Carbon::create( $this->year, $this->end['month'], $this->end['day'], 23, 59, 59 );
+		return Carbon::create( $this->year, $this->end['month'], $this->end['day'], 23, 59, 59.9999999 );
 	}
 
 	public function name() {
